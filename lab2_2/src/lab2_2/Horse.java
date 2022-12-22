3ac54d51353b62cc11d429f6deb5cb0a98d62dee
Soclassy <63253930+Soclassy@users.noreply.github.com>
2022-12-22 14:30:21 +0300

    Add files via upload

---
+++ lab2_2/src/lab2_2/Horse.java
@@ -0,0 +1,20 @@
+package lab2_2;
+
+
+public class Horse extends Animal{
+    public Horse(String food, String location){
+        super(food, location);
+    }
+
+    private double Speed;
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Ииииго-го!");
+    }
+
+    @Override
+    public void Eat() {
+        System.out.println("Лошадь ест " + this.getFood());
+    }
+}
